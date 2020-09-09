5fb111300780a3782699f6d2016fb478b0b2dd0b
Jeshta Bhoyedhur <jeshta@lsl.digital>
2020-09-09 14:23:19 +0400

    comments

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import (
 	"patterns/singleton"
 )
 
-func main() {
+func withInit() {
+	// Using Init() package
 	x := singleton.GetInstanceA()
 	y := singleton.GetInstanceA()
 	fmt.Println(x)
 	fmt.Println(y)
-
 	fmt.Println()
-	
+}
+
+func withSync() {
+	// Using sync package
 	fmt.Println("Using sync.Once()")
+
 	//First creation
 	stateManager := singleton.GetManager()
 	if stateManager.GetState() == "off" {
@@ -27,5 +31,10 @@ func main() {
 		stateManager1.SetState("off")
 	}
 	fmt.Println(stateManager1)
+}
 
+func main() {
+	// withInit()
+
+	withSync()
 }
